d1da856a80b8453133a8018c67a9152f404b9b40
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-09-19 17:06:22 +0200

    Update header of the matlab files

--- A.Y. 22-23/Practical Session 1/area_cylinder.m
+++ A.Y. 22-23/Practical Session 1/area_cylinder.m
@@ -1,3 +1,22 @@
+%-------------------------------------------------------------------------%
+%   __  __    _  _____ _        _    ____    _  _      ____    _ ____     %
+%  |  \/  |  / \|_   _| |      / \  | __ )  | || |    / ___|__| |  _ \    %
+%  | |\/| | / _ \ | | | |     / _ \ |  _ \  | || |_  | |   / _` | |_) |   %
+%  | |  | |/ ___ \| | | |___ / ___ \| |_) | |__   _| | |__| (_| |  __/    %
+%  |_|  |_/_/   \_\_| |_____/_/   \_\____/     |_|    \____\__,_|_|       %
+%                                                                         %
+%-------------------------------------------------------------------------%
+%                                                                         %
+%   Author: Timoteo Dinelli <timoteo.dinelli@polimi.it>                   %
+%   CRECK Modeling Group <http://creckmodeling.chem.polimi.it>            %
+%   Department of Chemistry, Materials and Chemical Engineering           %
+%   Politecnico di Milano                                                 %
+%   P.zza Leonardo da Vinci 32, 20133 Milano                              %
+%                                                                         %
+% ----------------------------------------------------------------------- %
+
+clear, clc
+
 D = 0.6; % m
 h = 0.5; % m
 
@@ -7,11 +26,6 @@ perimeter = pi * D;
 Area = perimeter * h;
 
 disp(Area);
-
-%% Clear the screen and the variables
-
-clc, clear
-
 %% Now we compute everything calling the function ...
 
 D = 5; % m
@@ -28,9 +42,11 @@ disp(Area2)
 
 function area = ComputeArea2(diameter, height)
 % ComputeArea is a supercool function that is needed by engineer to compute
+
 % the area of a cylinder given the diameter and the height.
 
 perimeter = pi * diameter;
 area = perimeter * height;
 
 end
+
