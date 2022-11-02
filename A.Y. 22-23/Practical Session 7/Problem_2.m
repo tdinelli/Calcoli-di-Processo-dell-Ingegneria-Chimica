07e0e200fec5a1d29d6be0a4e7657ed213c5145a
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-11-02 17:58:06 +0100

    Upload practical session 7

--- A.Y. 22-23/Practical Session 7/Problem_2.m
+++ A.Y. 22-23/Practical Session 7/Problem_2.m
@@ -16,6 +16,9 @@ options = optimset('Display','iter','PlotFcns', ...
 
 %% Solve the problem
 
+[sol_newton, error_newton] = Newton_numerical(310, @target,...
+    @forward_diff, 1e-8, 50);
+
 [sol_fsolve, fval_fsolve, exitFlag_fsolve] = fsolve(@target,...
                                                     310, options);
 
@@ -24,9 +27,14 @@ options = optimset('Display','iter','PlotFcns', ...
 figure(2)
 hold on
 plot(T_vect, P0vap, 'LineWidth', 2.5)
+plot([Tinf Tsup], [0 0], 'k--'); % plot the horizontal line
 scatter(sol_fsolve, fval_fsolve, 140, 'MarkerFaceColor', 'red')
+scatter(sol_newton(end), target(sol_newton(end)), 140,'MarkerFaceColor',...
+    'blue', 'MarkerEdgeColor','blue')
 ylabel('P^{0}_{vap} [Pa]', 'FontSize', 18)
 xlabel('T [K]', 'FontSize', 18)
+legend('P^{0}_{vap}', '', 'fsolve solution', 'Newton solution', ...
+    'FontSize', 18)
 
 %% Functions
 
